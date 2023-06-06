3e6ec963813e68605541340cd2ad7ca28ec6ee68
Mohamed Muhsin J <91737781+MohamedmuhsinJ@users.noreply.github.com>
2023-06-06 17:21:21 +0530

    inital commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/mohamedmuhsinJ/loginAssignment/Db"
+	"github.com/mohamedmuhsinJ/loginAssignment/controllers"
+)
+
+func init() {
+	db.ConnectToDb()
+	controllers.SyncDb()
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/register", controllers.Register)
+	router.Run(":8080")
+
+}
